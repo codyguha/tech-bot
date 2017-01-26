982a8a0af8b2174a25c5831c38c5312afe484f1a
codyguha <aalo.guha@gmail.com>
2017-01-26 10:19:36 -0800

    heroku

--- app/controllers/conversations.js
+++ app/controllers/conversations.js
@@ -277,8 +277,8 @@ function welcomeMessage(bot, incoming){
       bot.reply(incoming, {text: "Please choose to agree or disagree with the following statements."});
       setTimeout(function() {
         startSurvey(bot, incoming)
-      }, 3000)
-    }, 1000)
+      }, 1000)
+    }, 4000)
   }, 1000)
 }
 
